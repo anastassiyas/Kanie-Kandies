58972c99146fcabc0ba2323311a0e5f15672ba07
anastassiya.savostina <a.savostina@icloud.com>
2021-11-02 23:33:26 -0600

    Adding files

---
+++ app.js
@@ -0,0 +1,11 @@
+//Puppy Calculator
+function calculateDogAge(age) {
+    var humanYears=document.getElementById("solution").value;
+    var dogYears = 7*humanYears;
+    console.log("Your doggie is " + dogYears + " years old in dog years!");
+    document.getElementById("solution1").innerHTML= "Your doggie is " + dogYears + " years old in dog years!" ;
+    }
+    
+    calculateDogAge(1);
+    calculateDogAge(2);
+    calculateDogAge(5);
