0a9b1cffb0bec0faee082fce4db2ba77a24df519
anastassiya.savostina <a.savostina@icloud.com>
2021-11-11 19:12:11 -0700

    Adding files

--- app.js
+++ app.js
@@ -64,31 +64,51 @@ function special() {
 console.log(`You got a` + special());
 element.innerHTML=`Check out the Special today 👀` + `</br>` + special();
 
-let day;
-switch (new Date().getDay()) {
-    case 0:
-      day = "Brunch and free samples";
-      break;
-    case 1:
-      day = "1/2 off all oilsy";
-      break;
-    case 2:
-       day = "2-for-1 Buy-one-Get-one Free day";
-      break;
-    case 3:
-      day = "Buy-one-Get-one Free day";
-      break;
-    case 4:
-      day = "Add a gummy";
-      break;
-    case 5:
-      day = "Bring a friend get a extra treat";
-      break;
-    case 6:
-      day = "Buy two edibles get one free";
-  }
-  document.getElementById("message1").innerHTML = (`Today is ${day}`);
-  console.log("Today is " + day);
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//       day = "Brunch and free samples";
+//       break;
+//     case 1:
+//       day = "1/2 off all oilsy";
+//       break;
+//     case 2:
+//        day = "2-for-1 Buy-one-Get-one Free day";
+//       break;
+//     case 3:
+//       day = "Buy-one-Get-one Free day";
+//       break;
+//     case 4:
+//       day = "Add a gummy";
+//       break;
+//     case 5:
+//       day = "Bring a friend get a extra treat";
+//       break;
+//     case 6:
+//       day = "Buy two edibles get one free";
+//   }
+//   document.getElementById("message1").innerHTML = (`Today is ${day}`);
+//   console.log("Today is " + day);
+
+
+  const arrayCarousel = [
+      "images/cbd.png",
+      "images/cbd1.png",
+
+
+  ]
+  counter = 0
+
+  function setImage () {
+
+document.getElementById("carousel-cbd").src = arrayCarousel[counter];
+//document.getElementById("carousel-cbd1").src = arrayCarousel[counter];
+  
+
+counter = (counter+1) % arrayCarousel.length;
+}
+setInterval(setImage, 2000);
+
 
 
 //Array Literal
