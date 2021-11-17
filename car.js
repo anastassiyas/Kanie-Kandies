9333273ca516561ff1a91fcfa7837a158de651d2
anastassiya.savostina <a.savostina@icloud.com>
2021-11-16 23:18:16 -0700

    Adding files

--- car.js
+++ car.js
@@ -1,9 +1,3 @@
-var img=document.createElement("img");
-
-image.src= image.jpg;
-var src =document.getElementById("x");
-
-src.appendChild(img); 
 var rover = {
 make: "Tata Motors",
 model: "Land Rover Range Rover Sport",
@@ -12,60 +6,92 @@ available: true,
 description:" The mid-range P400e plug-in hybrid",
 price: "$71,000"
 }
-
-function dispay() {
-var place=document.getElementById("submit");
-for (value in rover) {
-place.innerHTML="Make:" + rover.make + '<br>' +
-"Model:" + rover.model + '<br>' +
-"Year:" + rover.year + '<br>' +
-"Available" + rover.available + '<br>' +
-"About" + rover.description + '<br>' +
-"Price" + rover.price;
-}  
-};
-
-var porsche = {
-make: "Volkswagen Group",
-model: "Porsche Cayenne",
-year: "2021",
-available:true,
-description:"include an impressive pair of plug-in hybrids",
-price: "$89,000"
-}
+function display1() {
+    var place = document.getElementById("submit1");
+    for (value in rover) {
+        place.innerHTML="Make:" + rover.make + '<br>' +
+        "Model: " + rover.model + '<br>' +
+        "Year: " + rover.year + '<br>' +
+        "Available: " + rover.available + '<br>' +
+        "About: " + rover.description + '<br>' +
+        "Price: " + rover.price;
+    }
+        };
+        function clear1() {
+            var place = document.getElementById("submit1");
+            place.innerHTML = "See Details";
+           
+        };
     
-function dispay2() {
-var place=document.getElementById("submit");
-for (value in porsche) {
-place.innerHTML="Make:" + porsche.make + '<br>' +
-"Model:" + porsche.model + '<br>' +
-"Year:" + porsche.year + '<br>' +
-"Available" + porsche.available + '<br>' +
-"About" + porsche.description + '<br>' +
-"price" + porsche.price;
-}  
-};
-
-var mercedes = {
-make: "Daimler AG",
-model: "Mercedes-Benz GLE 450",
-year: "2021",
-available:true,
-description:"The GLE 350de diesel plug-in hybrid",
-price: "$92,000"
-}
         
-function dispay3() {
-var place=document.getElementById("submit");
-for (value in mercedes) {
-place.innerHTML="Make:" + mercedes.make + '<br>' +
-"Model:" + mercedes.model + '<br>' +
-"Year:" + mercedes.year + '<br>' +
-"Available" + mercedes.available + '<br>' +
-"About" + mercedes.description  + '<br>' +
-"price" + mercedes.price;
-}  
-};
+    var porsche = {
+        make: "Volkswagen Group",
+        model: "Porsche Cayenne",
+        year: "2021",
+        available:true,
+        description:"include an impressive pair of plug-in hybrids",
+        price: "$89,000"
+        }
+            
+        function display2() {
+        var place = document.getElementById("submit2");              
+        for (value in porsche) {
+        place.innerHTML="Make:" + porsche.make + '<br>' +
+        "Model:" + porsche.model + '<br>' +
+        "Year:" + porsche.year + '<br>' +
+        "Available" + porsche.available + '<br>' +
+        "About" + porsche.description + '<br>' +
+        "price" + porsche.price;
+        } 
+        };
+        function clear2() {
+            var place = document.getElementById("submit2");
+            place.innerHTML = "See Details";
+           
+        };
+        var mercedes = {
+            make: "Daimler AG",
+            model: "Mercedes-Benz GLE 450",
+            year: "2021",
+            available:true,
+            description:"The GLE 350de diesel plug-in hybrid",
+            price: "$92,000"
+            }
+                    
+            function display3() {
+            var place = document.getElementById("submit3");
+            for (value in mercedes) {
+            place.innerHTML="Make:" + mercedes.make + '<br>' +
+            "Model:" + mercedes.model + '<br>' +
+            "Year:" + mercedes.year + '<br>' +
+            "Available" + mercedes.available + '<br>' +
+            "About" + mercedes.description  + '<br>' +
+            "price" + mercedes.price;
+            }  
+            };
+                
+            
+            
+            
+            
+
+    
+        function clear3() {
+        var place = document.getElementById("submit3");
+        place.innerHTML = "See Details";
+       
+    };
+    
+    
+    
+
+
+
+
+
+
+
+   
 
 
 
@@ -96,19 +122,3 @@ bird.color + " feathers.");
 
 describeBird(tommyBird );
 
-// The constructor
-function Book(title, author,numPages) {
-// The properties of this object
-this.title = title;
-this.author = author;
-this.numPages = numPages;
-this.currentPage = 0;
-}
-
-//A method of the object
-Book.prototype.read=function () {
-document.write("Title" + this.title + "Author" + this.title + "numpages" + this.numPages + "0" + this.currentPage)
-}
-
-var book1 = new Book("It", "Stephen King", 394, 2)
-book1.read()
