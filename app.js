0b90fea0ee4314ceeca1448214765c5b0c1d9f52
anastassiya.savostina <a.savostina@icloud.com>
2021-11-19 13:00:22 -0700

    Adding files

--- app.js
+++ app.js
@@ -108,9 +108,31 @@ counter = (counter+1) % arrayCarousel.length;
 }
 setInterval(setImage, 2000);
 
+//Create a table 
 
-
-
+function addTable() {
+    var myTableDiv = document.getElementById("myDynamicTable");
+  
+    var table = document.createElement('TABLE');
+    table.border = '1';
+  
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+  
+    for (var i = 0; i < 3; i++) {
+      var tr = document.createElement('TR');
+      tableBody.appendChild(tr);
+  
+      for (var j = 0; j < 4; j++) {
+        var td = document.createElement('TD');
+        td.width = '75';
+        td.appendChild(document.createTextNode("Cell " + i + "," + j));
+        tr.appendChild(td);
+      }
+    }
+    myTableDiv.appendChild(table);
+  }
+  addTable();
 
 
 
