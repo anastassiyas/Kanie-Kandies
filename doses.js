98cf529bd95e717eaf15cec3bd50f34fb3e49a54
anastassiya.savostina <a.savostina@icloud.com>
2021-11-20 00:36:08 -0700

    Adding files

--- doses.js
+++ doses.js
@@ -127,31 +127,6 @@ function display4() {
 
 
 
-//Create a table 
-
-// function addTable() {
-//     var myTableDiv = document.getElementById("myDynamicTable");
-  
-//     var table = document.createElement('TABLE');
-//     table.border = '1';
-  
-//     var tableBody = document.createElement('TBODY');
-//     table.appendChild(tableBody);
-  
-//     for (var i = 0; i < 3; i++) {
-//       var tr = document.createElement('TR');
-//       tableBody.appendChild(tr);
-  
-//       for (var j = 0; j < 4; j++) {
-//         var td = document.createElement('TD');
-//         td.width = '75';
-//         td.appendChild(document.createTextNode("Cell " + i + "," + j));
-//         tr.appendChild(td);
-//       }
-//     }
-//     myTableDiv.appendChild(table);
-//   }
-//   addTable();
 
 
 
