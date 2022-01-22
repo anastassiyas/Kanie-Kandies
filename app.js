87ffbb4dab7d1ff5bddb4f6da70231f4e410ea4e
anastassiya.savostina <a.savostina@icloud.com>
2022-01-22 00:48:01 -0700

    done

--- app.js
+++ app.js
@@ -64,31 +64,6 @@ function special() {
 console.log(`You got a` + special());
 element.innerHTML=`Check out the Special today 👀` + `</br>` + special();
 
-// let day;
-// switch (new Date().getDay()) {
-//     case 0:
-//       day = "Brunch and free samples";
-//       break;
-//     case 1:
-//       day = "1/2 off all oilsy";
-//       break;
-//     case 2:
-//        day = "2-for-1 Buy-one-Get-one Free day";
-//       break;
-//     case 3:
-//       day = "Buy-one-Get-one Free day";
-//       break;
-//     case 4:
-//       day = "Add a gummy";
-//       break;
-//     case 5:
-//       day = "Bring a friend get a extra treat";
-//       break;
-//     case 6:
-//       day = "Buy two edibles get one free";
-//   }
-//   document.getElementById("message1").innerHTML = (`Today is ${day}`);
-//   console.log("Today is " + day);
 
 
   const arrayCarousel = [
@@ -109,5 +84,79 @@ document.getElementById("carousel-cbd").src = arrayCarousel[counter];
 counter = (counter+1) % arrayCarousel.length;
 }
 setInterval(setImage, 2000);
+
+
+
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+}
+
+// Defining a function to validate form 
+function validateForm() {
+    // Retrieving the values of form elements 
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var mobile = document.contactForm.mobile.value;
+  
+    
+
+    // Defining error variables with a default value
+    var nameErr = emailErr = mobileErr = true;
+
+    // Validate name
+    if (name == "") {
+        printError("nameErr", "Please enter your Full name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;
+        if (regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
+
+    // Validate email address
+    if (email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        // Regular expression for basic email validation
+        var regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+
+    // Validate mobile number
+    if (mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+    } else {
+        var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        if (regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid mobile number");
+        } else {
+            printError("mobileErr", "");
+            mobileErr = false;
+        }
+    }
+
+       // Prevent the form from being submitted if there are any errors
+    if ((nameErr || emailErr || mobileErr) == true) {
+        return false;
+    } else {
+        // Creating a string from input data for preview
+        var dataPreview = "You've entered the following details: \n" +
+            "Full Name: " + name + "\n" +
+            "Email Address: " + email + "\n" +
+            "Mobile Number: " + mobile + "\n"; 
+        }
+        // Display input data in a dialog box before submitting the form
+        alert(dataPreview);
+    }
+
 
 
